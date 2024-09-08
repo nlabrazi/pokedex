69190a4da287de2b33992f2b3934a5bd7482cd1c
nlabrazi <na.labrazi@gmail.com>
2024-09-08 07:51:14 +0200

    feat: fix filename & dirname (eslint)

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,9 @@
+import { fileURLToPath } from 'url';
+import { dirname } from '@angular/compiler-cli';
 import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname, // Chemin racine du projet
